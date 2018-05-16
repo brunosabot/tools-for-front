27634b2d752c2e858fa5e34cfd0276b34619f037
Bruno Sabot <brunosabot@gmail.com>
2018-05-16 17:13:27 +0200

    🐛 ♻️  SVG graph are wrong calculated

--- src/routes/BezierCurves/components/CubicBezierSvg/index.jsx
+++ src/routes/BezierCurves/components/CubicBezierSvg/index.jsx
@@ -1,22 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const CubicBezierSvg = ({ x1, y1, x2, y2 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 110 110"
-    xmlSpace="preserve"
-    height="100px"
-    width="100px"
-  >
-    <path
-      stroke="#ffb010"
-      strokeWidth="4"
-      fill="none"
-      d={`M5 105C${x1 * 100 + 5} ${105 - y1 * 100},${105 - x2 * 100} ${y2 * 100 + 5},105 5`}
-    />
-  </svg>
-);
+const getPointX = (width, x) => x * width;
+
+// In this SVG y = 0 should be at the bottom,
+// so every y point is substracted from the height
+const getPointY = (height, y) => height - y * height;
+
+const withOffset = (offset, value) => offset + value;
+
+const CubicBezierSvg = ({ x1, y1, x2, y2 }) => {
+  const x0 = 0;
+  const y0 = 0;
+  const x3 = 1;
+  const y3 = 1;
+  const width = 100;
+  const height = 100;
+  const offset = 2;
+
+  const vwidth = width + offset * 2;
+  const vheight = height + offset * 2;
+
+  // Initial point
+  const p0x = withOffset(offset, getPointX(width, x0));
+  const p0y = withOffset(offset, getPointY(height, y0));
+
+  // Start transition point
+  const p1x = withOffset(offset, getPointX(width, x1));
+  const p1y = withOffset(offset, getPointY(height, y1));
+
+  // End transition point
+  const p2x = withOffset(offset, getPointX(width, x2));
+  const p2y = withOffset(offset, getPointY(height, y2));
+
+  // Final point
+  const p3x = withOffset(offset, getPointX(width, x3));
+  const p3y = withOffset(offset, getPointY(height, y3));
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${vwidth} ${vheight}`}
+      xmlSpace="preserve"
+      height={`${height}px`}
+      width={`${width}px`}
+    >
+      <path
+        stroke="#ffb010"
+        strokeWidth="3"
+        fill="none"
+        d={`M${p0x} ${p0y} C ${p1x} ${p1y}, ${p2x} ${p2y}, ${p3x} ${p3y}`}
+      />
+    </svg>
+  );
+};
 
 CubicBezierSvg.propTypes = {
   x1: PropTypes.number.isRequired,
